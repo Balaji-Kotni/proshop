6cde1363f981e278cc9ca58da3febb0f8333bac9
Balaji-Kotni <kotnivenkatabalaji@gmail.com>
2021-01-19 21:36:23 +0530

    redox integration

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,10 +1,16 @@
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from '../Constants/ProductConstants'
+
 export const productListReducer = (state = { products: [] }, action) => {
-  switch (DeviceAcceleration.type) {
-    case 'PRODUCT_LIST_REQUST':
-      return { loading: true, product: [] }
-    case 'PRODUCT_LIST_SUCCESS':
-      return { loading: false, product: action.payload }
-    case 'PRODUCT_FAIL':
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] }
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload }
+    case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
-
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
